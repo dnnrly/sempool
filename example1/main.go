f33129704db525ca0ac528f26d657c7105949c6f
Pascal Dennerly <dnnrly@gmail.com>
2018-10-03 23:35:11 +0100

    First example

---
+++ example1/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+    "log"
+    "time"
+)
+
+func main() {
+    start := time.Now()
+
+    // Imagine this is the result of a search on a REST API
+    results := []int{10, 15, 8, 3, 17, 20, 1, 6, 10, 9, 13, 19}
+
+    for _, d := range results {
+        // Imagine this is a long running operation, perhaps another
+        // REST API call
+	log.Printf("Waiting for %d seconds\n", d)
+	time.Sleep(time.Second * time.Duration(d))
+    }
+
+    log.Printf("Total time taken: %s\n", time.Now().Sub(start))
+}
+
+func seconds(secs int) time.Duration {
+    return time.Second * time.Duration(secs)
+}
+
